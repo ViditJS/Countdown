1b9fa324201fd3ed4aad225b381d3863a9cbdf76
Your Name <you@example.com>
2018-07-22 22:45:00 -0400

    timer is working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Clock from './clock'
 import './App.css';
 
 class App extends Component {
@@ -17,12 +17,7 @@ changeDeadline() {
     return (
       <div className="App">
         <div className="App-title">Countdown to {this.state.deadline}</div>
-          <div>
-            <div className="Clock-days"> 14 days</div>
-            <div className="Clock-hours">30 hours</div>
-            <div className="Clock-minutes">15 minutes</div>
-            <div className="Clock-seconds">20 seconds</div>
-          </div>
+          <Clock deadline = {this.state.deadline}/>
           <div>
             <input placeholder="new Date" onChange={event => this.setState({newDeadLine: event.target.value})}/>
             <button onClick={() => this.changeDeadline()}> Submit </button>
